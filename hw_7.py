339dc318a427ad6451786aa80bb4317a12077a09
Grib <super.kamrad@yandex.ru>
2022-09-05 23:01:48 +0500

    Задача 3 решена

--- hw_7.py
+++ hw_7.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 def add_ingidient(dish_recipes: dict,ingridient: str):
     item = ingridient.split("|")
     name = item[0]
@@ -40,6 +43,30 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
             shop_list[component_name] = component_info
         merge_shop_lists(result, shop_list)
     return result
+
+def get_file_content(file_path):
+    file_name = os.path.basename(file_path)
+    result = ""
+
+    with open(file_path,'r', encoding = 'utf-8') as file:
+        content = file.read()
+        lines_count = content.count('\n') + 1
+        result = f'{file_name}\n{lines_count}\n{content}\n'
+    return result
+
+def files_merge(src_directory, result_file):
+    files = sorted(Path(src_directory).glob('*[0-9].txt'))
+    file_content = []
+    print(files)
+    for file in files:
+        file_content.append(get_file_content(file))
+    
+    file_content.sort(key=lambda x: x.count('\n'))
+
+    with open(result_file,'w', encoding = 'utf-8') as file:
+        file.write(''.join(file_content))
+
+files_merge('.', 'result.txt')    
 
 
         
